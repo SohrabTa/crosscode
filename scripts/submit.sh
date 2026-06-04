b604800fafdbfad3525ecf8374638064afab317d
Sohrab Tawana <sohrab.tawana@tum.de>
2026-06-04 18:56:57 +0200

    chore: update directory mounting paths and configuration for data and checkpoints

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -11,14 +11,17 @@ CONFIG_FILE="training_config.yaml"
 # Define Mounts
 CODE_DIR="/dss/dsshome1/08/ga25ley2/code/crosscode"
 INTERPLM_DIR="/dss/dsshome1/08/ga25ley2/code/InterPLM"
-DATA_DIR="/dss/dssfs02/lwp-dss-0001/pn67na/pn67na-dss-0000/ga25ley2"
+DATA_DIR="/dss/dssfs02/lwp-dss-0001/pn67na/pn67na-dss-0000/ga25ley2/data"
+HF_HOME="/dss/dssfs02/lwp-dss-0001/pn67na/pn67na-dss-0000/ga25ley2/hf_home"
+MODEL_CHECKPOINTS_DIR="/dss/dssfs02/lwp-dss-0001/pn67na/pn67na-dss-0000/ga25ley2/model_checkpoints"
 
 # Mounts: Host:Container
-MOUNTS="${CODE_DIR}:/workspace/crosscode,${DATA_DIR}:/workspace/data,${INTERPLM_DIR}:/workspace/InterPLM"
+MOUNTS="${CODE_DIR}:/workspace/crosscode,${DATA_DIR}:/workspace/data,${INTERPLM_DIR}:/workspace/InterPLM,${HF_HOME}:/workspace/hf_home,${MODEL_CHECKPOINTS_DIR}:/workspace/model_checkpoints"
 
 # Env
 export WANDB_API_KEY=$(cat wandb/api_key)
-export HF_HOME="/workspace/data/hf_home"
+export HF_HOME="/workspace/hf_home"
+export MODEL_CHECKPOINTS_DIR="/workspace/model_checkpoints"
 export PYTHONPATH="/workspace/crosscode"
 
 mkdir -p logs
